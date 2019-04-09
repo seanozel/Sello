86edee710a6ab87a6007794f258a23d91f3f1b03
Sean Ozel <seanozel@gmail.com>
2019-04-10 09:35:24 +1000

    Initial Commit

---
+++ Sello/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  Sello
+//
+//  Created by Helen on 10/4/19.
+//  Copyright © 2019 Helen. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
